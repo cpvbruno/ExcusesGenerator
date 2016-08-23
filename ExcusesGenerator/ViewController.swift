efe755dab1e99924e17a016e1abba8148eb7e884
Bruno Gonçalves <bruno.ing879@gmail.com>
2016-08-23 08:58:10 -0300

    Initial Commit

---
+++ ExcusesGenerator/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ExcusesGenerator
+//
+//  Created by Bruno Gonçalves on 23/08/16.
+//  Copyright © 2016 Bruno Pantaleão. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
